4044bb379aeb52e86bf13d30c76c6f9c2b3b9df6
AleksanderKr <95710989+AleksanderKr@users.noreply.github.com>
2023-11-19 10:35:51 +0100

    Finalize login form

--- app/src/main/java/com/example/titanapi/controllers/RequestLogin.kt
+++ app/src/main/java/com/example/titanapi/controllers/RequestLogin.kt
@@ -12,18 +12,20 @@ import retrofit2.Response
 object RequestLogin {
     private val api = UserModule.provideLoginApi()
     private val logData = LoginData("titan", "titan123")
+    var access_token = ""
+    var refresh_token = ""
     fun sendLoginRequest() {
     api.loginUser(logData).enqueue(object : Callback<UserData> {
         override fun onResponse(call: Call<UserData>, response: Response<UserData>) {
             if (response.isSuccessful) {
-                val access_token = response.body()?.accessToken.toString()
-                val refresh_token = response.body()?.refreshToken.toString()
+                access_token = response.body()?.accessToken.toString()
+                refresh_token = response.body()?.refreshToken.toString()
                 Log.d("TAG", access_token)
             }
         }
 
         override fun onFailure(call: Call<UserData>, t: Throwable) {
-            Log.d("TAG", "fail")
+            Log.d("TAG", "login failed")
         }
 
     })
